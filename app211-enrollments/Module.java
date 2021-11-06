8b466449484dac389f74bf6a403f00ab7f599842
AkbarHumza <22138564@bucks.ac.uk>
2021-11-06 10:40:43 +0000

    Create Module.java

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (Humza Akbar)
+ * @version (06/11/2021)
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private String code;
+   
+    private String title;
+    
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String code, String title)
+    {
+        this.code = code;
+        this.title = title; 
+        
+    }
+
+   
+}
